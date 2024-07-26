4bf560f099a636ec1e57fe5b689fcfcbfe5d89c4
utsavudhungana <139304818+utsavudhungana@users.noreply.github.com>
2024-07-26 17:32:54 -0500

    Update dynamiccurltest.sh

--- dynamiccurltest.sh
+++ dynamiccurltest.sh
@@ -6,10 +6,18 @@ api_version=2020-12-01
 source_endpoint=https://$source_workspace_name.dev.azuresynapse.net
 target_endpoint=https://$workspace_name.dev.azuresynapse.net
 
+if [[ $1 == "sqlScript" ]]; then
+aztype="sql-script"
+elif [[ $1 == "linkedservice" ]]; then
+aztype="linked-service"
+elif [[ $1 == "sparkJobDefinition" ]]; then
+aztype="spark-job-definition"
+else
+aztype=$1
+fi
 
 # List artifact type and store in an array, removing spaces
-#mapfile -t var_array < <(az synapse $aztype list --workspace-name $source_workspace_name --query "[].name" -o tsv | sed 's/ //g')
-mapfile -t var_array < <((curl -s -X GET -H "Authorization: Bearer $(az account get-access-token --resource=https://dev.azuresynapse.net/ --query accessToken --output tsv)" $source_endpoint/$1s?api-version=$api_version | jq -r '.value[].name') | sed 's/ //g')
+mapfile -t var_array < <(az synapse $aztype list --workspace-name $source_workspace_name --query "[].name" -o tsv | sed 's/ //g')
 
 # Loop over the array to get each artifact type and save as .json file
 for var in "${var_array[@]}"; 
